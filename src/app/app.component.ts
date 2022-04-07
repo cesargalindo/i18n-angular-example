5d52bae0ccee06a2a5c97241ee1ae8922e3d03d7
Cesar Galindo <cesar.galindo@irhythmtech.com>
2022-04-07 11:44:02 -0700

    capture router paths Angular sees

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, NavigationEnd, NavigationStart, RoutesRecognized, Params } from '@angular/router';
 
 import { TitleService } from './services/title/title/title.service';
+
+// export basePathArrays = []
+
+export enum EContent {
+    // Text / Heading
+    US = 'en-US',
+    FR = 'fr',
+    DE = 'de',
+    JA = 'ja',
+}
+
+
+export const Content: { [contentName: string]: boolean } = {
+    // Text
+    [EContent.US]: true,
+    [EContent.FR]: true,
+    [EContent.DE]: true,
+    [EContent.JA]: true,
+}
 
 @Component({
     selector: 'app-root',
@@ -8,12 +28,70 @@ import { TitleService } from './services/title/title/title.service';
 })
 export class AppComponent implements OnInit {
 
+    // Check first navigationStart
+    private navStartCheck = false;
+
     // Select first tab
     tabSelected = 0;
 
     xmessage = $localize`:@@xmessage:Message X`;
 
-    public constructor(public titleService: TitleService) { }
+    public constructor(
+        public titleService: TitleService,
+		private router: Router,
+    ){
+
+        // checks routes for data flags
+		// this.router.events
+        // .pipe(
+        //     filter((event) => event instanceof NavigationEnd),
+        //     tap(ev => this.tmpRouterEvent = ev),
+        //     map(() => this.route),
+        //     map((route1) => {
+        //         // Check params
+        //         route1.queryParams.subscribe((params: Params) => {
+        //             // If url has domain, assume user came from SSO clicklet - autoredirect to Okta
+        //             if (params['domain']) {
+        //                 this.oktaChickletRedirect = true;
+        //             }
+        //         });
+
+        //         while (route1.firstChild) {
+        //             route1 = route1.firstChild;
+        //         }
+        //         return route1;
+        //     }),
+        //     mergeMap((route2) => route2.data))
+        // .subscribe((data) => {
+        //     // google analytics code for tracking each page navigation
+        //     gtag('config', 'UA-7181233-4', {
+        //         'page_path': this.tmpRouterEvent.urlAfterRedirects
+        //     });
+        //     this.whiteBackground = (data.whiteBackground && data.whiteBackground === true) ? true : false;
+        //     this.hideHeader = (data.hideHeader && data.hideHeader === true) ? true : false;
+
+        //     // If true do not check data, SSO domain param set it to true im map above
+        //     if (this.oktaChickletRedirect) {
+        //         this.hideFooter = true;
+        //     }
+        //     else {
+        //         this.hideFooter = (data.hideFooter && data.hideFooter === true) ? true : false;
+        //     }
+
+        // });
+
+        router.events.subscribe((event) => {
+            console.log(event);
+            let foo = event instanceof NavigationStart;
+            console.log(foo);
+            if (!this.navStartCheck && foo) {
+                let moo = event['url'].split('/');
+                console.log('======', event['url'], '0', moo[0], '1', moo[1], '=', Content[ moo[1]]);
+                this.navStartCheck = true;
+            }
+          });
+
+    }
 
     public ngOnInit(): void {
         // Initilize text on app.component.html for first tab
